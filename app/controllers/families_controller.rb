c551d53bf4e09780d9e120917f8c9a06e23c3466
cookmilk <liu.chang@berkeley.edu>
2016-04-15 20:37:13 +0000

    Filter Almost

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -7,6 +7,65 @@ class FamiliesController < ApplicationController
   
   def index
     @families = Family.order(:name)
+    # if (params[:status] && @families.collect(&:status).include?(params[:status]))
+    #   @family = Family.send(params[:status])
+    # else
+    #   @family = Family.all
+    # end
+    
+    # if params[:status] == nil
+    #   @family = Family.stu
+    # else
+    #   @family = Hash[params[:status]].keys
+    # end
+    
+
+    new_params = {}
+    need_redirect = false
+    if params[:status].nil? && !session[:status].nil?
+      new_params[:status] = session[:status]
+      need_redirect = true
+    end
+    
+    # Set sessions
+    if !params[:status].nil?
+      session[:status] = params[:status]
+      new_params[:status] = params[:status]
+    end
+
+    
+    if need_redirect
+      redirect_to family_path(new_params)
+    end
+
+
+
+    # @family = params[:status].nil? ? Family.all : Family.where(rating: params[:status].keys)
+    # @checked = params[:status].nil? ? @families : params[:status].keys
+
+    
+    # @all_status = Family.all_status
+    # @status = @all_status
+
+    # if params.fetch("status", false)
+    #   @status = params[:status].keys
+    # end
+
+    # sess_status = session.fetch("all_status", @all_status)
+
+    # if (@status == @all_status and
+    #     (sess_status != @all_status))
+    #   flash.keep
+    #   ratings_params = Hash[*sess_status.collect {|k| [k, 'yes']}.flatten]
+    #   return redirect_to family_path(nil, {:status => ratings_params})
+    # end
+
+    # @movies = Family.where(:rating => @status)
+
+    # session[:movies_ratings] = @status
+    
+    
+    
   end
   
   def edit 
--- app/models/family.rb
+++ app/models/family.rb
@@ -1,4 +1,18 @@
 class Family < ActiveRecord::Base
   validates :name, :presence => true
   has_many :comments, :dependent => :destroy
+  
+  def self.stu
+        status = Array.new
+        i = 0
+        self.select(:status).each do |stu|
+            status[i] = stu.status
+            i += 1;
+        end
+        puts "=================== #{status} ============"
+        return status.sort
+  end
+  def self.all_status
+        ["New","Longtime","Non","Prospective","Friend of BE","Ex","Deceased"]
+  end
 end
