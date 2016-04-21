d0b81d706d11ff0efcf2fc0d3e8eca512456ecac
thexienator <jasonxie9801@gmail.com>
2016-04-21 21:44:10 +0000

    stop letting non-admins edit themselves to admin

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
   
   def index
     if !current_user.admin?
-      @comments = Comment.where("confidential = ? OR (confidential = ? AND user_id = ?)", false, true, current_user.id).order(:created_at).reverse
+      @comments = Comment.where("confidential = ? OR user_id = ?", false, current_user.id).order(:created_at).reverse
     else
       @comments = Comment.order(:created_at).reverse
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :name, :email, :presence => true
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, 
+         :rememberable, :trackable, :validatable
   has_many :comments, :dependent => :destroy
   has_many :tasks, :dependent => :destroy
   
